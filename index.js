0881e46c1093eda7aa77cecb812e7b5c6dcb0a69
pgap22 <gerardo.saz120@gmail.com>
2024-06-06 21:05:52 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+import express from "express"
+import { Server } from "socket.io";
+import {createServer} from "http"
+import cors from "cors"
+import { logTime } from "./logTime.js";
+import { prisma } from "./prisma.js";
+
+const app = express()
+const server = createServer(app)
+app.use(cors())
+
+const io = new Server(server, {cors:{
+    origin: "*"
+}})
+
+app.get("/", (req,res)=>{
+    return res.send("splitq socket io")
+})
+
+
+server.listen("4000", ()=>{
+    console.log("Server socket io Running")
+})
+
+io.on('connection', (socket) => {
+    logTime("Connected:"+ socket.handshake.address)
+
+    socket.on("get_balance", async (id)=>{
+        try {
+            socket.join(id)
+            logTime("ROOM: "+id + " " + socket.handshake.address)
+            const { balance } = await prisma.users.findFirst({
+                where: {
+                    id
+                },
+                select: {
+                    balance: true
+                }
+            })
+            socket.to(id).emit("current_balance",balance)
+        } catch (error) {
+            console.log(error)
+        }
+    })
+
+    socket.on("user_data", (id)=>{
+        logTime("ROOM: "+id + " " + socket.handshake.address)
+        socket.join(id)
+    })
+    socket.on("recharge", (data)=>{
+        socket.to(data.room).emit("recive_recharge", data.recharge)
+        socket.to(data.room).emit("add_balance", data)
+    })
+});
